3b46dbed6fc4f6683082bd20a296fe4190694781
AKMunny <sodp5@naver.com>
2019-06-11 13:35:10 +0900

    init proj CommunicateWebViewExample

---
+++ CommunicateWebViewExample/app/src/main/java/com/example/communicatewebviewexample/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.communicatewebviewexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView wvWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wvWebView = findViewById(R.id.wvWebView);
+
+        wvWebView.setWebChromeClient(new WebChromeClient());
+        wvWebView.setWebViewClient(new MyWebClient());
+
+        wvWebView.getSettings().setJavaScriptEnabled(true);
+
+        wvWebView.addJavascriptInterface(new Object() {
+            @JavascriptInterface
+            public String get(String keyword) {
+                Toast.makeText(MainActivity.this, "call get method " + keyword, Toast.LENGTH_SHORT).show();
+                return "succeed";
+            }
+
+            @JavascriptInterface
+            public String put(String keyword) {
+                Toast.makeText(MainActivity.this, "call put method " + keyword, Toast.LENGTH_SHORT).show();
+                return "succeed";
+            }
+        }, "communicate");
+
+        wvWebView.loadUrl("file:///android_asset/www/index.html");
+    }
+
+    private class MyWebClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            return super.shouldOverrideUrlLoading(view, request);
+        }
+    }
+}
